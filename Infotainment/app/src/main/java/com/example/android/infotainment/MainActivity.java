b0999d106ed9df562e74b18250465544fcaea525
StuartCalverley <StuartCalverley13@gmail.com>
2016-10-26 09:28:02 -0400

    Made tab host so the tabs were at the botton of the screen

--- Infotainment/app/src/main/java/com/example/android/infotainment/MainActivity.java
+++ Infotainment/app/src/main/java/com/example/android/infotainment/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.infotainment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
 public class MainActivity extends AppCompatActivity {
 
