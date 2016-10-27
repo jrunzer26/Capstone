e437869334d8599439a8bc57f4cf6b6a186ac900
StuartCalverley <StuartCalverley13@gmail.com>
2016-10-27 17:37:48 -0400

    Rough draft tabs are all working and display what they should, need to fix the formatting on the picutres.

--- Infotainment/app/src/main/java/com/example/android/infotainment/MainActivity.java
+++ Infotainment/app/src/main/java/com/example/android/infotainment/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.infotainment;
 
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost host = (TabHost) findViewById(R.id.tabhost_mainactivity_tabs);
+        host.setup();
+
+        TabHost.TabSpec gpsTab = host.newTabSpec("GPS");
+        gpsTab.setContent(R.id.gpsTab_linear);
+        gpsTab.setIndicator("GPS");
+        host.addTab(gpsTab);
+
+        TabHost.TabSpec radioTab = host.newTabSpec("Radio");
+        radioTab.setContent(R.id.radioTab_linear);
+        radioTab.setIndicator("Radio");
+        host.addTab(radioTab);
+
+        TabHost.TabSpec menuTab = host.newTabSpec("Menu");
+        menuTab.setContent(R.id.menuTab_linear);
+        menuTab.setIndicator("Menu");
+        host.addTab(menuTab);
     }
 }
