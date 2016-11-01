5eaf858d1ece0558e5eae38fa5af2f61a0279465
jrunzer26 <jason.runzer@uoit.net>
2016-10-31 23:09:11 -0400

    added bluetooth handlers

--- Infotainment/app/src/main/java/com/example/android/infotainment/DataAnalyst.java
+++ Infotainment/app/src/main/java/com/example/android/infotainment/DataAnalyst.java
@@ -4,9 +4,17 @@ package com.example.android.infotainment;
  * Created by 100520993 on 10/31/2016.
  */
 
-public class DataAnalyst implements DataReceiver {
+public class DataAnalyst implements DataReceiver, Runnable {
     @Override
     public void onReceive(String data) {
         // TODO: 10/31/2016 implement processing of watch and car data 
+    }
+
+    /**
+     * Analyze the heart rate and car data.
+     */
+    @Override
+    public void run() {
+
     }
 }
---
+++ Infotainment/app/src/main/java/com/example/android/infotainment/DataParser.java
@@ -0,0 +1,34 @@
+package com.example.android.infotainment;
+
+/**
+ * Created by 100520993 on 10/31/2016.
+ */
+
+import java.util.ArrayList;
+
+/** Parses the data from the watch heart rate and the car data **/
+public class DataParser implements DataReceiver {
+    private DataReceiver dataReceiver;
+    // buffers
+    ArrayList<String> carData;
+    ArrayList<String> heartRateData;
+
+    /**
+     * Constructs the Parser
+     * @param dataReceiver the analyst to receive the two parts
+     */
+    public DataParser(DataReceiver dataReceiver) {
+        this.dataReceiver = dataReceiver;
+        // TODO: 10/31/2016
+    }
+
+    /**
+     * Save data from one of the bluetooth handlers into buffer,
+     * send if both peaces are present.
+     * @param data
+     */
+    @Override
+    public void onReceive(String data) {
+        // TODO: 10/31/2016
+    }
+}
