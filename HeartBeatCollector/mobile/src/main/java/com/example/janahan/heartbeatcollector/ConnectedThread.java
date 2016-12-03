869a8ba6cc6f57c07159768527dadf1eaacf7170
StuartCalverley <StuartCalverley13@gmail.com>
2016-12-02 20:02:25 -0500

    Added most comments to the heartBeatCollect project

--- HeartBeatCollector/mobile/src/main/java/com/example/janahan/heartbeatcollector/ConnectedThread.java
+++ HeartBeatCollector/mobile/src/main/java/com/example/janahan/heartbeatcollector/ConnectedThread.java
@@ -17,37 +17,56 @@ import java.io.OutputStream;
 
 public class ConnectedThread extends Thread {
 
+    //Declares the input and output streams and socket
     private final BluetoothSocket mmSocket;
     private final InputStream mmInStream;
     private final OutputStream mmOutStream;
 
+    /**
+     * Initializes the input and output stream
+     * @param socket - The socket the client is sending and receiving on
+     */
     public ConnectedThread(BluetoothSocket socket){
         mmSocket = socket;
         InputStream tmpIn = null;
         OutputStream tmpOut = null;
 
         try {
+            //Stores the input stream and output stream into tmpIn and tmpOut
             tmpIn = socket.getInputStream();
             tmpOut = socket.getOutputStream();
         } catch (IOException e) {}
 
+        //After successfully being able to store the streams into the tmp variables store them into
+        //The variables you declared at the start
         mmInStream = tmpIn;
         mmOutStream = tmpOut;
     }
 
+    /**
+     * Reads messages from the inputStream
+     */
     public void run() {
         byte[] buffer = new byte[1024];
         int bytes;
 
+        //Always running
         while (true) {
             try {
+                //Holds up here until there is something to read
                 bytes = mmInStream.read(buffer);
+                //Converts the message from bytes into a string
                 String readMessage = new String(buffer, 0, bytes);
                 System.out.println(readMessage);
             } catch (Exception e) {}
         }
     }
 
+    /**
+     * This method is called from the Simulator class and passes in the byte array of all the values
+     * from the simulator interface every 5 seconds
+     * @param bytes the message you are sending to the server
+     */
     public void write(byte[] bytes) {
         try {
             Log.e("Writing", "Message");
@@ -55,6 +74,9 @@ public class ConnectedThread extends Thread {
         } catch (IOException e) { }
     }
 
+    /**
+     * Closes the bluetooth socket
+     */
     public void cancel() {
         try {
             mmSocket.close();
